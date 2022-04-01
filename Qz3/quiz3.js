bd9bca25a4e0b8dcc817f6813d34f4efea7633dc
kklucy11 <kklucyhuang@gmail.com>
2022-04-02 02:06:58 +0800

    123

--- Qz3/quiz3.js
+++ Qz3/quiz3.js
@@ -2,6 +2,6 @@ var enter = document.getElementById('enter');
 document.addEventListener('keydown', logkey);
 function logkey(a) {
     if (65 <= a.keyCode && a.keyCode <= 112) {
-        enter.textContent += `${a.keyCode}`;
+        enter.textContent += `${a.key}`;
     }
 }
